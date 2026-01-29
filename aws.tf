5b3653950a6b8214c68b4ced77a2ebdcc7d90766
Hamdigabsi <gabsi.hamdi@gmail.com>
2026-01-29 12:57:06 +0100

    Create aws.tf

---
+++ aws.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1" # Irlande
+}
+
+resource "aws_instance" "demo_vm" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "demo-terraform-vm"
+  }
+}
